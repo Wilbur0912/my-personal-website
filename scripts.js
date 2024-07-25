881c1e443c366f93a554c5a30d929951c6a23087
Mosh Mamedani <wilbur9912@gmail.com>
2024-07-25 23:45:46 +0800

    portforlio initial

--- scripts.js
+++ scripts.js
@@ -18,15 +18,13 @@ $(document).ready(function(){
             }
         }
     });
-  });
-
-  document.addEventListener("DOMContentLoaded", function() {
-    const menuToggle = document.getElementById("mobile-menu");
-    const nav = document.querySelector("nav ul");
-    
-    menuToggle.addEventListener("click", function() {
-        nav.classList.toggle("showing");
-    });
 });
 
-  
+document.addEventListener("DOMContentLoaded", function() {
+    const menuToggle = document.getElementById('mobile-menu');
+    const nav = document.querySelector('nav ul');
+
+    menuToggle.addEventListener('click', function() {
+        nav.classList.toggle('active');
+    });
+});
